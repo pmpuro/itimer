09b9fc758947f478c83d393cd32184a3c932da8c
Panu Puro <panu.puro@elisa.fi>
2020-05-22 21:14:46 +0300

    Modified the timer implementation to use the stream controller.

--- lib/mytimer.dart
+++ lib/mytimer.dart
@@ -1,16 +1,57 @@
+import 'dart:async';
+
 class MyTimer {
   int _duration = 0;
+  Timer _timer;
+  StreamController<int> _streamController;
+
+  void _onListen() {}
+
+  void _onPause() {
+    _cancelTimer();
+  }
+
+  void _onResume() {
+    _startTimer();
+  }
+
+  void _onCancel() {
+    _cancelTimer();
+  }
+
+  void _startTimer() {
+    _streamController = StreamController<int>(
+        onListen: _onListen,
+        onPause: _onPause,
+        onResume: _onResume,
+        onCancel: _onCancel);
+    _timer = Timer.periodic(Duration(seconds: 1), _work);
+  }
+
+  void _cancelTimer() {
+    if (_timer != null) {
+      _timer.cancel();
+      _timer = null;
+    }
+  }
 
   void start(int duration) {
     _duration = duration;
+    _startTimer();
   }
 
-  Stream<int> output() async* {
-    while (_duration >= 0) {
-      yield _duration--;
+  Future<void> _work(Timer t) async {
+    if (_duration >= 0) {
+      _streamController.add(_duration);
+      --_duration;
       await Future.delayed(Duration(seconds: 1));
+    } else {
+      _cancelTimer();
+      _streamController.close();
     }
   }
+
+  Stream<int> output() => _streamController.stream;
 
   Stream<bool> active() async* {
     var previousCondition = false;
